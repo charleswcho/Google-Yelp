45e56797a71d2cdefb8cc1cad694a80fb21458dd
Charles <charles.c@leantaas.com>
2019-05-03 11:09:40 -0700

    Minor CSS tweaks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import SearchBar from './components/SearchBar';
+
+import './App.sass';
+
+/* global google */
 
 function App() {
+  search('restaurant', 'San Francisco');
+
+  function search(query, place) {
+    const geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({ address: place }, (results, status) => {
+      if (status === google.maps.GeocoderStatus.OK) {
+        const center = {
+          lat: results[0].geometry.location.lat(),
+          lng: results[0].geometry.location.lng()
+        };
+
+        getLocationData(query, center);
+      }
+    });
+  }
+
+  function getLocationData(query, center) {
+    if (query === undefined || query === '') {
+      query = 'restaurant';
+    }
+
+    const mapOptions = {
+      center: { lat: 37.7749, lng: -122.4194 },
+      zoom: 14,
+      scrollwheel: false,
+      mapTypeControl: false,
+      zoomControl: true,
+      zoomControlOptions: {
+        position: google.maps.ControlPosition.LEFT_TOP
+      },
+      scaleControl: false,
+      streetViewControl: false
+    };
+
+    const map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    const request = {
+      location: center,
+      radius: '500',
+      query: query
+    };
+
+    const service = new google.maps.places.PlacesService(map);
+
+    service.textSearch(request, (res) => {
+      console.log(res);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar />
+
+      <div id="map" class="google-map" />
     </div>
   );
 }
