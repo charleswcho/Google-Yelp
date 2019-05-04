81913293706153ce0cdcdd131b14ef7cff9bebf1
Charles <charles.c@leantaas.com>
2019-05-04 11:51:23 -0700

    Updates

---
+++ precache-manifest.8a4cb3e8225e8dd1efa1b5501d9fa371.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73ace4906c01a6ed209ee59a0e252c75",
+    "url": "/google-yelp/index.html"
+  },
+  {
+    "revision": "b44aac7b08387e37db96",
+    "url": "/google-yelp/static/css/main.7302d0ab.chunk.css"
+  },
+  {
+    "revision": "a5f310f5b874bc42913c",
+    "url": "/google-yelp/static/js/2.9bc57c2a.chunk.js"
+  },
+  {
+    "revision": "b44aac7b08387e37db96",
+    "url": "/google-yelp/static/js/main.97d544b1.chunk.js"
+  },
+  {
+    "revision": "0068fdcbefc83198c707",
+    "url": "/google-yelp/static/js/runtime~main.d1110ff9.js"
+  }
+]);
